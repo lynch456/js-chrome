087f766039e6cedbcfa726711088a5e283aa1137
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-15 00:42:13 +0900

    9/15 const, let
    never var

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 console.log(12345);
 console.log("kimkimkim");
-// const =  변수 선언
+// const =  변수 선언, 상수(값이 바뀔 수 없음)
+// let = 변수 선언, 선언 후 업데이트 가능, 15번 줄   //const와 let는 변수의 보호를 위해 만들어짐.
+// var = 초창기부터 있던 변수 선언, 변수 재선언 가능
+// 기본적으로 const/ 필요할 때 let/ var는 지양
 const a = 10;
 const b = 2;
-const myName = " kim";
+let myName = " kim";
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello" + myName);
+
+myName = "ik soo";
+
+console.log("yout new name is " + myName);
