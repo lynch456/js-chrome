574c3607533ad85617398e4c238a88cf3b0294f2
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-15 00:22:13 +0900

    9/15 variables

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("hi");
+console.log(12345);
+console.log("kimkimkim");
+// const =  변수 선언
+const a = 10;
+const b = 2;
+const myName = " kim";
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
