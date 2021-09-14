061ba43573827ae761157a5aabd3cae2d3503d2d
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-15 02:14:49 +0900

    9/15 arrays

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-// data type
-// Boolean = true or false
-// null = 아무것도 없음으로 채움, 자연적으로 발생하지 않음. 변수 안에 어떤 것이 없다는 것을 확실히 하기 위해 씀.
-const amIFat = null;
-let something; //variables는 존재해도 값이 정의되지 않음 / 출력 = undefined
-console.log(amIFat);
-console.log(something);
+// arrays 배열
+// arrays [] //list //목적은 하나의 variables 안에 데이터의 list를 가지는 것.
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// 요소 가지고 오기 [요소]
+console.log(daysOfWeek[4]);
+// 요소 더하기
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+console.log(toBuy);
+console.log(toBuy[2]);
