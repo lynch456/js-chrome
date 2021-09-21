1c6613c138d6f929f1a822ebe5ccdc4486821662
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-21 15:16:33 +0900

    9/21 functions 2

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-function sayHello() {
-  console.log("Hello my name is C");
+function sayHello(nameOfPerson, age) {
+  console.log("Hello my name is" + nameOfPerson + " and I'm " + age);
 }
 
-sayHello();
-sayHello();
-sayHello();
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
+}
+function divide(a, b) {
+  console.log(a / b);
+}
+plus(8, 60);
+divide(98, 20);
+
+const player = {
+  name: "nico",
+  sayHello: function (otherPersonsName) {
+    console.log("hello " + otherPersonsName + " nice to meet you");
+  },
+};
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
