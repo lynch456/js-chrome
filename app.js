d5052807b6abf4585154ebb294966b7f989415e5
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-26 21:12:46 +0900

    9/26 conditionals

--- app.js
+++ app.js
@@ -40,8 +40,14 @@
 
 const age = parseInt(prompt("how old are you?"));
 
-if (isNaN(age)) {
-  console.log("please write a number");
-} else {
-  console.log("thank you");
+if (isNaN(age) || age < 0) {
+  console.log("please write a real positive number");
+} else if (age < 18) {
+  console.log("you are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("you can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("you should exercise");
+} else if (age > 80) {
+  console.log("you can do whatever you want");
 }
