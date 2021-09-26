b7989dbd14a7c614d339628c209b043609552f2f
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-26 23:02:02 +0900

    9/26 event 1

--- app.js
+++ app.js
@@ -1,53 +1,19 @@
-// const age = 96;
-// function calculateKrAge(ageOfForeigner) {
-//   ageOfForeigner + 2;
-//   return "hello";
-// }
-// const krAge = calculateKrAge(age);
+const title = document.querySelector("div.hello:first-child h1");
 
-// console.log(krAge);
+console.dir(title);
 
-// const calculator = {
-//   plus: function (a, b) {
-//     return a + b;
-//   },
-//   minus: function (a, b) {
-//     return a - b;
-//   },
-//   div: function (a, b) {
-//     return a / b;
-//   },
-//   times: function (a, b) {
-//     return a * b;
-//   },
-//   pow: function (a, b) {
-//     return a ** b;
-//   },
-// };
-// const plusResult = calculator.plus(2, 3);
-// const minusResult = calculator.minus(plusResult, 10);
-// const timesResult = calculator.times(10, minusResult);
-// const divResult = calculator.div(timesResult, plusResult);
-// const powResult = calculator.pow(divResult, minusResult);
-
-// const calculator = {
-//   plus: function (a, b) {
-//     return a + b;
-//   },
-// };
-// const plusResult = calculator.plus(2, 3);
-// console.log(plusResult);
-
-const age = parseInt(prompt("how old are you?"));
-
-if (isNaN(age) || age < 0) {
-  console.log("please write a real positive number");
-} else if (age < 18) {
-  console.log("you are too young");
-} else if (age >= 18 && age <= 50) {
-  console.log("you can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("you should exercise");
-} else if (age > 80) {
-  console.log("you can do whatever you want");
+function handleTitlsClick() {
+  title.style.color = "blue";
+  console.log("title was clicked!");
 }
+function handleMouseEnter() {
+  title.innerText = "mouse is here";
+  console.log("mouse is here");
+}
+function handleMouseLeave() {
+  title.innerText = "mouse is gone";
+  console.log("mouse is gone");
+}
+title.addEventListener("click", handleTitlsClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
