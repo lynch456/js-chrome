3c5bac2de053b083aeaf3746900a5677d38bf6b2
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-28 08:18:58 +0900

    css in js
    classList.toggle

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitlsClick() {
   const clickedClass = "clicked";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
+  h1.classList.toggle(clickedClass);
 }
 h1.addEventListener("click", handleTitlsClick);
 
 // className 모든 클래스 교체
 // classList 선택한 클래스 교체
+// classList.toggle classList에 클래스가 있는지 확인해서
+// 클래스가 있다면 제거해주고 존재하지 않는다면 classList에 class추가
