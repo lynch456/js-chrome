0d4de3633c3ff13389a945fa6f0f037988cee1b5
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-28 08:13:04 +0900

    css in js
    remain class name 1

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitlsClick() {
-  const clickedClass = "clicked sexy-font";
-  if (h1.className === clickedClass) {
-    h1.className = "";
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.className = clickedClass;
+    h1.classList.add(clickedClass);
   }
 }
 h1.addEventListener("click", handleTitlsClick);
+
+// className 모든 클래스 교체
+// classList 선택한 클래스 교체
