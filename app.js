1b4f46ad1196edd076dac5d3c5113118d9715f71
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-28 08:39:32 +0900

    9/28 login form 1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitlsClick() {
-  const clickedClass = "clicked";
-  h1.classList.toggle(clickedClass);
+function onLoginBtnClick() {
+  const username = loginInput.value;
+  if (username === "") {
+    alert("Please write your name");
+  } else if (username.length > 15) {
+    alert("Your name is too long");
+  }
 }
-h1.addEventListener("click", handleTitlsClick);
-
-// className 모든 클래스 교체
-// classList 선택한 클래스 교체
-// classList.toggle classList에 클래스가 있는지 확인해서
-// 클래스가 있다면 제거해주고 존재하지 않는다면 classList에 class추가
+loginButton.addEventListener("click", onLoginBtnClick);
