050fd282252e5eb8a4e21aea06ada8f42e8be132
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2021-09-28 07:44:05 +0900

    css is js

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitlsClick() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
-function handleMouseEnter() {
-  h1.innerText = "mouse is here";
-}
-function handleMouseLeave() {
-  h1.innerText = "mouse is gone";
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-  alert("copier!");
-}
-function handleWindowOffline() {
-  alert("SOS no WIFE");
-}
-function handleWindowOnline() {
-  alert("ALL GOOD");
-}
+
 h1.addEventListener("click", handleTitlsClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
